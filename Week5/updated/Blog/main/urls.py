1e53ea41396aa3195849bdcb3d0492179dab57bf
daniyarka <daniyar_k-98@mail.ru>
2018-10-11 08:52:27 +0600

    week5_update_done

--- Week5/updated/Blog/main/urls.py
+++ Week5/updated/Blog/main/urls.py
@@ -5,7 +5,9 @@ urlpatterns = [
     path('', views.show_posts),
     path('<int:post>/post', views.post_more),
     path('create_post', views.create_post),
-    path('<int:comment>/create_comment', views.create_comment),
+    path('<int:post>/create_comment', views.create_comment),
     path('<int:post>/delete_post', views.delete_post),
     path('<int:post>/update_post', views.update_post),
+    path('<int:post>/delete_comment/<int:comment>', views.delete_comment),
+    path('<int:post>/update_comment/<int:comment>', views.update_comment),
 ]
--- Week5/updated/Blog/main/views.py
+++ Week5/updated/Blog/main/views.py
@@ -62,22 +62,45 @@ def update_post(request, post):
     return render(request, 'update_post.html', context)
 
 
-def create_comment(request, comment):
+def create_comment(request, post):
     if request.method == 'POST':
         form = CommentForm(request.POST)
         if form.is_valid():
-            form.save()
             content = form.cleaned_data['content']
             author = form.cleaned_data['author']
             comment = Comment()
             comment.author = author
             comment.content = content
-            comment.post_id = Post.objects.get(pk=comment)
+            comment.post_id = Post.objects.get(pk=post)
             comment.save()
-            return redirect('..')
+            return redirect('./post')
     else:
         form = CommentForm()
     context = {
         'form': form
     }
     return render(request, 'create_comment.html', context)
+
+
+def delete_comment(request, comment, post):
+    del_list = Comment.objects.get(pk=comment)
+    del_list.delete()
+    return redirect('../post')
+
+def update_comment(request, post, comment):
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        print(form.is_valid())
+        if form.is_valid():
+            comment = Comment.objects.get(pk=comment)
+            comment.author = form.cleaned_data['author']
+            comment.content = form.cleaned_data['content']
+            comment.save()
+            return redirect('../post')
+    else:
+        form = CommentForm()
+    context = {
+        'form': form,
+        'comment': Comment.objects.get(pk=comment)
+    }
+    return render(request, 'update_comment.html', context)
